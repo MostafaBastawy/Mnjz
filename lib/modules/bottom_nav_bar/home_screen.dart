88d65a07dca19cc23143d0d62e8d825db73e6cdf
mostafabastawy <mostafa.bastawy@gmail.com>
2022-02-06 02:31:24 +0200

    first init

--- lib/modules/bottom_nav_bar/home_screen.dart
+++ lib/modules/bottom_nav_bar/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mnjz/cubit/cubit.dart';
@@ -349,7 +350,194 @@ class _HomeScreenState extends State<HomeScreen>
                       ),
                     ),
                   ),
-                  Container(),
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 394.w,
+                          height: 40.h,
+                          decoration: BoxDecoration(
+                            color: const Color(0xAF007AFC),
+                            borderRadius: BorderRadius.circular(4.r),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '+  اضف متجرك الان',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.sp,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20.h),
+                        Container(
+                          padding: const EdgeInsets.all(0),
+                          height: 42.h,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4.r),
+                            color: const Color(0xAFFAFAFA),
+                          ),
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.search,
+                                size: 20,
+                                color: Color(0xAF7F8083),
+                              ),
+                              SizedBox(width: 10.w),
+                              SizedBox(
+                                width: 138.w,
+                                height: 25.h,
+                                child: TextFormField(
+                                  controller: searchController,
+                                  keyboardType: TextInputType.phone,
+                                  maxLines: 1,
+                                  obscureText: false,
+                                  onChanged: (value) {},
+                                  onFieldSubmitted: (value) {},
+                                  validator: (value) {},
+                                  onTap: () {},
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'ابحث عن متجر (12)',
+                                    hintStyle: TextStyle(
+                                      fontSize: 12.sp,
+                                      fontWeight: FontWeight.w400,
+                                      color: const Color(0xAF7F8083),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 20.h),
+                        Expanded(
+                          child: ListView.separated(
+                            itemCount: 10,
+                            itemBuilder: (BuildContext context, int index) =>
+                                SizedBox(
+                              width: 394.w,
+                              height: 223.h,
+                              child: Column(
+                                children: [
+                                  Stack(
+                                    alignment: AlignmentDirectional.topCenter,
+                                    children: [
+                                      Container(
+                                        width: 394.w,
+                                        height: 145.h,
+                                        color: Colors.white,
+                                        child: Align(
+                                          alignment:
+                                              AlignmentDirectional.topCenter,
+                                          child: Container(
+                                            width: 394.w,
+                                            height: 113.h,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(8.r),
+                                              image: const DecorationImage(
+                                                image: AssetImage(
+                                                  'assets/images/1.png',
+                                                ),
+                                                fit: BoxFit.fill,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        left: 20,
+                                        top: 75,
+                                        child: Container(
+                                          height: 66.h,
+                                          width: 66.w,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(10.r),
+                                            image: const DecorationImage(
+                                              image: AssetImage(
+                                                'assets/images/2.png',
+                                              ),
+                                              fit: BoxFit.fill,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        right: 0,
+                                        top: 120,
+                                        child: Text(
+                                          'One way',
+                                          style: TextStyle(
+                                            fontSize: 16.sp,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 20.h),
+                                  Row(
+                                    children: [
+                                      RatingBar.builder(
+                                        ignoreGestures: true,
+                                        glowColor: Colors.red,
+                                        unratedColor: Colors.amber[100],
+                                        itemSize: 15,
+                                        initialRating: 5,
+                                        minRating: 1,
+                                        direction: Axis.horizontal,
+                                        allowHalfRating: true,
+                                        itemCount: 5,
+                                        itemPadding: const EdgeInsets.symmetric(
+                                            horizontal: 4.0),
+                                        itemBuilder: (context, _) => const Icon(
+                                          Icons.star,
+                                          color: const Color(0xAFFCD200),
+                                        ),
+                                        onRatingUpdate: (rating) {},
+                                      ),
+                                      Text(
+                                        '(3)',
+                                        style: TextStyle(
+                                          fontSize: 10.sp,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      const FaIcon(
+                                        FontAwesomeIcons.carAlt,
+                                        color: Color(0xAFC71717),
+                                        size: 17,
+                                      ),
+                                      SizedBox(width: 10.w),
+                                      Text(
+                                        'سيارات ومركبات . القاهرة . الإعلانات 32',
+                                        style: TextStyle(
+                                          fontSize: 12.sp,
+                                          color: const Color(0xAF7F8083),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            separatorBuilder:
+                                (BuildContext context, int index) => SizedBox(
+                              height: 10.h,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                   Container(),
                 ],
               ),
